180ed50f94db675a9cf11b0ed428ca17a34b407c
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-07-22 09:48:30 +0200

    Added imports.

---
+++ flickr8k.py
@@ -0,0 +1,27 @@
+import concurrent.futures
+import collections
+import dataclasses
+import hashlib
+import itertools
+import json
+import math
+import os
+import pathlib
+import random
+import re
+import string
+import time
+import urllib.request
+
+import einops
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from PIL import Image
+import requests
+import tqdm
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text
+import tensorflow_datasets as tfds
