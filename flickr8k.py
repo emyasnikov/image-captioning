0181da187ae6651b0491bcb50e3ff01e2695facc
Evgenij Myasnikov <evgenij.myasnikov@gmail.com>
2023-07-22 09:58:30 +0200

    Added flickr8k function.

--- flickr8k.py
+++ flickr8k.py
@@ -25,3 +25,40 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
 import tensorflow_datasets as tfds
+
+def flickr8k(path="flickr8k"):
+    path = pathlib.Path(path)
+
+    if len(list(path.rglob("*"))) < 16197:
+        tf.keras.utils.get_file(
+            origin="https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip",
+            cache_dir=".",
+            cache_subdir=path,
+            extract=True,
+        )
+        tf.keras.utils.get_file(
+            origin="https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip",
+            cache_dir=".",
+            cache_subdir=path,
+            extract=True,
+        )
+
+    captions = (path/"Flickr8k.token.txt").read_text().splitlines()
+    captions = (line.split("\t") for line in captions)
+    captions = ((filename.split("#")[0], caption) for (filename, caption) in captions)
+
+    cap_dict = collections.defaultdict(list)
+
+    for filename, caption in captions:
+        cap_dict[filename].append(caption)
+
+    train_files = (path/"Flickr_8k.trainImages.txt").read_text().splitlines()
+    train_captions = [(str(path/"Flickr8k_Dataset"/filename), cap_dict[filename]) for filename in train_files]
+
+    test_files = (path/"Flickr_8k.testImages.txt").read_text().splitlines()
+    test_captions = [(str(path/"Flickr8k_Dataset"/filename), cap_dict[filename]) for filename in test_files]
+
+    train_ds = tf.data.experimental.from_list(train_captions)
+    test_ds = tf.data.experimental.from_list(test_captions)
+
+    return train_ds, test_ds
